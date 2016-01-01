9867f0a110fdc7769aefccbb3bb9a36d3ecbd2cb
Alexssandro Oliveira <Alexssandro_gr@yahoo.com.br>
2016-01-01 16:38:23 -0300

    alterations in the 'Grupo' screen.

--- indicador/src/main/java/com/vitoria/controller/GrupoBean.java
+++ indicador/src/main/java/com/vitoria/controller/GrupoBean.java
@@ -1,21 +1,31 @@
 package com.vitoria.controller;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.DataModel;
 
 import com.vitoria.dao.GrupoDao;
 import com.vitoria.model.Grupo;
+import com.vitoria.util.FacesUtils;
 
 @ManagedBean
-@SessionScoped
-public class GrupoBean {
+@ViewScoped
+public class GrupoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	private Grupo grupo ;
 
 	private List<Grupo> grupos ;
 	
+	
+	public void novo(){
+		grupo = new Grupo();
+	}
 	
 	public GrupoBean() {
 		this.grupo = new Grupo();
@@ -37,16 +47,35 @@ public class GrupoBean {
 	public void setGrupos(List<Grupo> grupos) {
 		this.grupos = grupos;
 	}
+	
 
 	
 	public void adiciona(){
 		GrupoDao dao = new GrupoDao();
 		dao.adiciona(this.grupo);
 		this.grupo = new Grupo();
+		new FacesUtils().adcionaMensagemSucesso("Grupo adicionado com sucesso !");
+		
+	}
+	public void lista(){
+		GrupoDao dao = new GrupoDao();
+		this.grupos = dao.lista();
+	}
+	
+	public void remover(Grupo grupo){
+		GrupoDao dao = new GrupoDao();
+		dao.remove(grupo);
+		this.grupos = dao.lista();
+		new FacesUtils().adcionaMensagemSucesso("Grupo removido com sucesso !");
 		
 	}
 	
-	
+	public void edita(){
+		GrupoDao dao = new GrupoDao();
+		dao.atualiza(grupo);
+		new FacesUtils().adcionaMensagemSucesso("Grupo atualizado com sucesso !");		
+		
+	}
 	
 
 }
